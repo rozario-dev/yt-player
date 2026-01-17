50cedad937ce800cb5b953cd927ca4f43c7feedf
guqianfeng <guqianfeng@pro-m4.local>
2026-01-17 14:06:28 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import VideoInput from './components/VideoInput/VideoInput';
 import VideoPlayer from './components/VideoPlayer/VideoPlayer';
 import PlayerControls from './components/PlayerControls/PlayerControls';
